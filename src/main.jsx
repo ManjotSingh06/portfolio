b16a4508d5d55c2f4084ac184685cb3896e27cf2
ManjotSingh06 <mssmart6@gmail.com>
2026-09-21 02:40:32 +0530

    Added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import { createRoot } from "react-dom/client";
 import {
   ArrowUpRight, Github, Linkedin, Mail, Download, Terminal,
